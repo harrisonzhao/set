d5370a3f64a2a51b1725912306fb6eb626e22da5
Zhao Harrison <zhao3@ice11.ee.cooper.edu>
2014-05-08 21:24:41 -0400

    update

--- client/src/main/java/Set_GUI/Lobby.java
+++ client/src/main/java/Set_GUI/Lobby.java
@@ -165,9 +165,9 @@ public class Lobby extends JPanel {
     
     //image for header
     //BufferedImage header = null;
-	BufferedImage header;
-	Boolean image_succeed = true;
-	JLabel headerLabel;
+  BufferedImage header;
+  Boolean image_succeed = true;
+  JLabel headerLabel;
     try {
       // Here's code for getting the current working directory, but i'm not sure
       // if we need that. the hard coded one works fine for now. It may be an issue
@@ -378,13 +378,7 @@ public class Lobby extends JPanel {
           gameNameField.setText("");
           break;
         }
-<<<<<<< HEAD
       } while(redo);     
-=======
-      } while(redo);
-      maxPlayerField.setText("");
-      gameNameField.setText("");      
->>>>>>> b8ca97b5ccd22ae6c7a3eb95cd436fe013cd2ac7
       if(!exitCreate) {
         callingObj.sendMessageToServer("N~"+gameNameField.getText()+"~"+maxPlayerField.getText()); 
         login_Frame.enterGame();
